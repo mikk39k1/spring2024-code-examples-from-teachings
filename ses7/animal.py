0b091650b8575a8f714f8002b03922b70146795c
mikk39k1 <114911882+mikk39k1@users.noreply.github.com>
2024-03-11 08:13:56 +0000

    Stash

---
+++ ses7/animal.py
@@ -0,0 +1,13 @@
+class Animal:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+
+    def get_info(self):
+        return self
+    
+dog = Animal("Fido", 12)
+
+
+
